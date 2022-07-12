b7bfde6e420cd3a645be7297a1d962a0392af601
paulmaru <paul.mtz@gmail.com>
2022-07-12 09:52:42 -0700

    Subida de prácticas CSS

---
+++ PracticasCSS/javascript/index.js
@@ -0,0 +1,37 @@
+var modal = $('#modal');
+
+$(function(){
+    $('img').click(function(){
+        var idImg = $(this).attr("id");
+        console.log(idImg);
+        var srcImg = $(this).attr("src");
+        console.log(srcImg);
+        showModal(idImg, srcImg);
+    });
+    hideModal();
+
+    //NavBar
+    $('.fa-bars').click(function(){
+        $('nav ul li').addClass("show");
+        $('.fa-xmark').css("display","block");
+        $('.fa-bars').css("display","none");
+    });
+    $('.fa-xmark').click(function(){
+        $('nav ul li').removeClass("show");
+        $('.fa-bars').css("display","block");
+        $('.fa-xmark').css("display","none");
+    });
+});
+
+function showModal(idImg, srcImg){
+    var imagen = idImg;
+    var src = srcImg;
+    $('#imgModal').attr("src", srcImg);
+    modal.css("display", "block");
+}
+
+function hideModal(){
+    $('.cerrar').click(function(){
+        modal.css("display", "none");
+    });
+}
